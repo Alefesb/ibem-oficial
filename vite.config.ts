3ca5686b28362d60322c0a1d5f682d2a7cb1c73d
Alefe Santana <142458915+Alefesb@users.noreply.github.com>
2026-07-18 10:47:24 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import tailwind from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
+    tailwind(),
     tanstackStart({
       server: { entry: "server" },
     }),
