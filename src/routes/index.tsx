b8bdd726bcc0ba3bbf8136f2388025c97bcf133c
Alefe Santana <142458915+Alefesb@users.noreply.github.com>
2026-07-19 14:55:41 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -368,7 +368,7 @@ function Localizacao() {
             <div className="rounded-xl overflow-hidden border border-border shadow-[var(--shadow-elegant)] min-h-[380px]">
               <iframe
                 title="Mapa IBEM"
-                src="https://www.google.com/maps?q=Igreja+Batista&output=embed"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.56566889664!2d-38.4753719!3d-12.9356134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161bdc43986937%3A0x303b2132618a5b7f!2sIGREJA%20BATISTA%20MEDABEREL!5e0!3m2!1spt-BR!2sbr!4v1784483035134!5m2!1spt-BR!2sbr"
                 className="w-full h-full min-h-[380px]"
                 loading="lazy"
               />
